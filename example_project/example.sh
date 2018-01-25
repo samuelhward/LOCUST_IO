9cf18438cc44f070167a4f57e9d234fcd5155e29
Samuel Ward <shw515@york.ac.uk>
2018-01-25 17:30:15 +0000

    added example_project/example.sh - very simple example on how to run this at shell level

---
+++ example_project/example.sh
@@ -0,0 +1,13 @@
+#!/bin/env/bash
+
+imasdb test #set the location of the IDS database
+
+#kinplot -r run_number -s shot_number #make some nice IDS plots
+#scenplot -r run_number -s shot_number
+
+./example.py
+
+#point LOCUST to files here using symlink
+
+#make FLAGS='' #compile and run LOCUST
+#locust
