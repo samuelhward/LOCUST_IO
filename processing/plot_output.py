b73b2363c972f1aede47aa2b7a031a86ecb2ae30
Samuel Ward <shw515@york.ac.uk>
2018-01-25 17:18:15 +0000

    re-organised processing/ folder to have small modules to plot/process input/output

---
+++ processing/plot_output.py
@@ -0,0 +1,24 @@
+#plot_output.py
+
+'''
+Samuel Ward
+25/1/2018
+----
+plotting routines for LOCUST output data
+---
+notes:
+	https://stackoverflow.com/questions/9455111/python-define-method-outside-of-class-definition
+---
+'''
+
+def plot_orbit():
+
+	
+
+
+
+#################################
+
+##################################################################
+
+###################################################################################################
