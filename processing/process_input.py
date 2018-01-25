65e243fcc3aacda4c46673a0a587e1997e7c7320
Samuel Ward <shw515@york.ac.uk>
2018-01-25 17:18:15 +0000

    re-organised processing/ folder to have small modules to plot/process input/output

---
+++ processing/process_input.py
@@ -0,0 +1,28 @@
+#process_input.py
+
+'''
+Samuel Ward
+25/1/2018
+----
+routines for processing LOCUST_IO input
+---
+notes:
+	https://stackoverflow.com/questions/9455111/python-define-method-outside-of-class-definition
+---
+'''
+
+
+def blah():
+
+
+	
+
+
+
+
+
+#################################
+
+##################################################################
+
+###################################################################################################
---
+++ processing/process_output.py
@@ -0,0 +1,23 @@
+#process_output.py
+
+'''
+Samuel Ward
+25/1/2018
+----
+routines for processing LOCUST_IO output
+---
+notes:
+	https://stackoverflow.com/questions/9455111/python-define-method-outside-of-class-definition
+---
+'''
+
+
+
+
+	
+
+#################################
+
+##################################################################
+
+###################################################################################################
