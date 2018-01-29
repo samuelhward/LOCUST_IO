0a9c4b43534660d7878e23eee93675fccb98b29e
Samuel Ward <shw515@york.ac.uk>
2018-01-29 09:50:43 +0000

    added plot_number_density() and plot_equilibrium()

--- processing/plot_input.py
+++ processing/plot_input.py
@@ -7,62 +7,110 @@ Samuel Ward
 plotting routines for LOCUST input data
 ---
 notes:
-	https://stackoverflow.com/questions/9455111/python-define-method-outside-of-class-definition
+    https://stackoverflow.com/questions/9455111/python-define-method-outside-of-class-definition
 ---
 '''
 
+##################################################################
+#Preamble
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+##################################################################
+#Main Code
 
 
 
 
 
-def plot_equilibria():
+def plot_number_density(some_number_density):
+    """
+    simple number density plot
+     
+    notes:
+    """
 
-	#0D data
+    plt.plot(some_number_density['psi'],some_number_density['n'])
+    plt.show()
 
-	print(test.data['nh'])		#number of points in R (x or width)
-	print(test.data['nw'])		#number of points in Z (y or height)
-	print(test.data['idum']) 	#number of spatial dimensions?
-	print(test.data['rdim'])	#size of the R dimension in m
-	print(test.data['zdim'])	#size of the Z dimension in m
-	print(test.data['rcentr'])	#reference value of R
-	print(test.data['bcentr'])	#vacuum toroidal magnetic field at rcentr
-	print(test.data['rleft'])	#R at left (inner) boundary
-	print(test.data['zmid'])	#Z at middle of domain
-	print(test.data['rmaxis'])	#R at magnetic axis (O-point)
-	print(test.data['zmaxis'])	#Z at magnetic axis (O-point)
-	print(test.data['simag'])	#poloidal flux psi at magnetic axis (Weber / rad)
-	print(test.data['sibry'])	#poloidal flux at plasma boundary (Weber / rad)
-	print(test.data['current'])	#plasma current [Amps]   
-	print(test.data['xdum'])	#dummy variable - just contains zero
-	print(test.data['nbbbs'])	#plasma boundary
-	print(test.data['limitr'])	#wall boundary
 
-	#1D data
-	plt.plot(test.data['fpol'])		#poloidal current function on uniform flux grid (1D array of f(psi)=R*Bt  [meter-Tesla])
-	plt.plot(test.data['pres'])		#plasma pressure in nt/m^2 on uniform flux grid (1D array of p(psi) [Pascals])
-	plt.plot(test.data['ffprime'])	#workk1
-	plt.plot(test.data['pprime'])	#workk1
-	plt.plot(test.data['qpsi'])		#q values on uniform flux grid
-	plt.plot(test.data['rlim']) 	#r wall boundary
-	plt.plot(test.data['zlim'])		#z wall boundary
-	plt.plot(test.data['rbbbs'])	#r plasma boundary
-	plt.plot(test.data['zbbbs'])	#z plasma boundary
 
-	#2D data
-	import matplotlib.pyplot as plt
-	from matplotlib import cm #get colourmaps
-	from mpl_toolkits import mplot3d #import 3D plotting axes
-	fig=plt.figure()
 
-	X=numpy.arange(test.data['nh']) #make a mesh
-	Y=numpy.arange(test.data['nw'])
-	X,Y=numpy.meshgrid(X,Y)
-	Z=test.data['psirz'] #2D array (nx,ny) of poloidal flux
-	ax=plt.axes(projection='3d')
-	#ax.view_init(elev=90, azim=None) #rotate the camera
-	ax.plot_surface(X,Y,Z,rstride=1,cstride=1,cmap='viridis',edgecolor='none',linewidth=0,antialiased=True,vmin=0.99*numpy.amin(Z),vmax=1.01*numpy.amax(Z))
-	plt.show()
+
+
+
+def plot_equilibrium(some_equilibrium,key=None,boundary=None):
+    """
+    simple equilibrium plot 
+     
+    notes:
+    """
+
+    #default plot mode (plot psirz grid)
+    if key==None:
+
+        import matplotlib.pyplot as plt
+        from matplotlib import cm #get colourmaps
+        from mpl_toolkits import mplot3d #import 3D plotting axes
+        fig=plt.figure()
+
+        X=np.arange(some_equilibrium['nw']) #make a mesh
+        Y=np.arange(some_equilibrium['nh']) 
+        X,Y=np.meshgrid(X,Y) #NOTE change this to R_mesh Z_mesh after issue 23 is fixed
+        Z=some_equilibrium['psirz'].T 
+        #plt.contour(X,Y,Z,cmap='viridis',edgecolor='none',linewidth=0,antialiased=True,vmin=0.99*np.amin(Z),vmax=1.01*np.amax(Z))
+        plt.pcolormesh(X,Y,Z,cmap='viridis',edgecolor='none',linewidth=0,antialiased=True,vmin=0.99*np.amin(Z),vmax=1.01*np.amax(Z))
+
+        plt.colorbar()
+
+    else:
+        
+        #0D data
+        if some_equilibrium[key].ndim==0:
+            print(some_equilibrium[key])
+        
+        #1D data
+        elif some_equilibrium[key].ndim==1:
+            plt.plot(some_equilibrium[key])
+
+        #2D data
+        elif some_equilibrium[key].ndim==2:
+
+            import matplotlib.pyplot as plt
+            from matplotlib import cm #get colourmaps
+            from mpl_toolkits import mplot3d #import 3D plotting axes
+            fig=plt.figure()
+
+            X=np.arange(some_equilibrium['nw']) #make a mesh
+            Y=np.arange(some_equilibrium['nh']) 
+            X,Y=np.meshgrid(X,Y) #NOTE change this to R_mesh Z_mesh after issue 23 is fixed
+            Z=some_equilibrium['psirz'].T #2D array (nw,nh) of poloidal flux
+            
+            #2D plot
+            #plt.contour(X,Y,Z,cmap='viridis',edgecolor='none',linewidth=0,antialiased=True,vmin=0.99*np.amin(Z),vmax=1.01*np.amax(Z))
+            plt.pcolormesh(X,Y,Z,cmap='viridis',edgecolor='none',linewidth=0,antialiased=True,vmin=0.99*np.amin(Z),vmax=1.01*np.amax(Z))
+
+            #3D plot
+            #ax=plt.axes(projection='3d')
+            #ax.view_init(elev=90, azim=None) #rotate the camera
+            #ax.plot_surface(X,Y,Z,rstride=1,cstride=1,cmap='viridis',edgecolor='none',linewidth=0,antialiased=True,vmin=0.99*np.amin(Z),vmax=1.01*np.amax(Z))
+            
+            plt.colorbar()
+
+
+    #add boundaries if requested
+    if boundary=='limiters':
+        pass
+        #plt.plot(some_equilibrium['rlim'],some_equilibrium['zlim']) #NOTE waiting for issue 23 fix
+
+    elif boundary=='plasma':
+        pass
+        #plt.plot(some_equilibrium['rbbbs'],some_equilibrium['zbbbs']) #NOTE waiting for issue 23 fix
+
+
+    plt.show()
 
 
 
