129feb074a4f921f452658c9a8b368ed1694052d
Samuel Ward <shw515@york.ac.uk>
2018-01-29 09:51:05 +0000

    added RZ plane orbit plotting

--- processing/plot_output.py
+++ processing/plot_output.py
@@ -7,14 +7,34 @@ Samuel Ward
 plotting routines for LOCUST output data
 ---
 notes:
-	https://stackoverflow.com/questions/9455111/python-define-method-outside-of-class-definition
+    https://stackoverflow.com/questions/9455111/python-define-method-outside-of-class-definition
 ---
 '''
 
-def plot_orbit():
+##################################################################
+#Preamble
+import matplotlib.pyplot as plt
+import numpy as np
 
-	
 
+
+##################################################################
+#Main Code
+
+
+def plot_orbits(some_orbits,particles):
+    """
+    simple orbits plot in the R,Z plane
+     
+    notes:
+        particles needs to be iterable list of particle numbers
+    """
+    
+    for particle in particles:
+
+        plt.plot(some_orbits['orbits'][:,particle,0],some_orbits['orbits'][:,particle,2])
+
+    plt.show()
 
 
 #################################
