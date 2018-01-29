df2e5c3a936f3db5f7e4cb986862b7a72e96ce0e
Samuel Ward <shw515@york.ac.uk>
2018-01-29 10:01:22 +0000

    made to work with the sample data

--- example_project/example.py
+++ example_project/example.py
@@ -6,7 +6,7 @@
 Samuel Ward
 24/01/2018
 ----
-very simple example project script for converting a set of IDSs into the LOCUST inputs
+very simple example project script for manipulating the sample input/output data
 ---
 notes:
 
@@ -22,18 +22,34 @@ import sys
 import numpy
 
 try:
-	import context
+    import context
 except:
-	raise ImportError("ERROR: local context.py not found - please create one and add it to this directory\nreturning\n")
-	sys.exit(1)
+    raise ImportError("ERROR: local context.py not found - please create one and add it to this directory\nreturning\n")
+    sys.exit(1)
 
 try: 
-	import input_classes
+    import input_classes
 except:
-	raise ImportError("ERROR: LOCUST_IO/classes/input_classes.py could not be imported!\nreturning\n")
-	sys.exit(1)
+    raise ImportError("ERROR: LOCUST_IO/classes/input_classes.py could not be imported!\nreturning\n")
+    sys.exit(1)
 
+try: 
+    import output_classes
+except:
+    raise ImportError("ERROR: LOCUST_IO/classes/output_classes.py could not be imported!\nreturning\n")
+    sys.exit(1)
 
+try: 
+    import plot_input
+except:
+    raise ImportError("ERROR: LOCUST_IO/processing/plot_input.py could not be imported!\nreturning\n")
+    sys.exit(1)
+
+try: 
+    import plot_output
+except:
+    raise ImportError("ERROR: LOCUST_IO/processing/plot_output.py could not be imported!\nreturning\n")
+    sys.exit(1)
 
 
 
@@ -41,36 +57,19 @@ except:
 #Main Code
 
 
+#read in the sample input data
+my_Ne=input_classes.Number_Density(ID='LOCUST_IO sample Ne profile',data_format='ASCII',input_filename='sample_ne_profile.file',properties='electrons')
 
-#read in all the required data
-
-equilibrium=input_classes.Equilibrium(ID='that awesome Jet equilibrium',data_format='IDS',shot=1,run=7) 
-
-particles=input_classes.Beam_Deposition(ID='that awesome ASDEX-U NBI deposition',data_format='IDS',shot=2,run=8)
-
-temperature_i=input_classes.Temperature(ID='that awesome KSTAR ion temperature profile',data_format='IDS',shot=3,run=9,properties='ions') 
-
-temperature_e=input_classes.Temperature(ID='that awesome MAST-U electron temperature profile',data_format='IDS',shot=4,run=10,properties='electrons')
-
-density_i=input_classes.Number_Density(ID='that awesome DIII-D ion density profile',data_format='IDS',shot=5,run=11,properties='ions')
-
-density_e=input_classes.Number_Density(ID='that awesome ITER electron density profile',data_format='IDS',shot=6,run=12,properties='electrons')
+#see what it looks like
+plot_input.plot_number_density(my_Ne)
 
 
-#dump all the data to the LOCUST formats 
 
-equilibrium.dump_data(data_format='GEQDSK',output_filename='my_equilibrium.eqdsk') 
+#read in the sample output data
+my_orbits=output_classes.Orbits(ID='LOCUST_IO sample orbits',data_format='ASCII',input_filename='sample_orbits.file')
 
-particles.dump_data(data_format='ASCII',output_filename='ptcles.dat')
-
-temperature_i.dump_data(data_format='ASCII',output_filename='profile_Ti.dat')
-
-temperature_e.dump_data(data_format='ASCII',output_filename='profile_Te.dat')
-
-density_i.dump_data(data_format='ASCII',output_filename='profile_ni.dat')
-
-density_e.dump_data(data_format='ASCII',output_filename='profile_ne.dat')
-
+#plot particle 0's trajectory in the R,Z plane
+plot_output.plot_orbits(my_orbits,[0])
 
 
 #################################
