a04d2dccf777f099d43e327e40285f47a43b88a4
Samuel Ward <shw515@york.ac.uk>
2018-02-12 14:24:29 +0000

    support.py added import numpy as numpy, numpy version, added list of data required for running LOCUST for use with run_check

--- classes/support.py
+++ classes/support.py
@@ -16,25 +16,11 @@ notes:
 #Preamble
 import sys
 import os
+import numpy as np
 
 
 ##################################################################
 #Project directory paths
-
-
-
-
-
-
-#get the paths to folders within the package for importing modules, opening files etc
-pwd=os.path.dirname(os.path.abspath(__file__)) #get the directory this script is in
-
-dir_locust_io=os.path.dirname(pwd) #go one level up from that
-
-dir_input_files=os.path.join(dir_locust_io,'input_files/') #add the / at the end so the user only needs to append filenames
-dir_output_files=os.path.join(dir_locust_io,'output_files/')
-dir_classes=os.path.join(dir_locust_io,'classes/')
-
 
 
 
@@ -47,6 +33,21 @@ python_version=sys.version_info
 
 
 
+#get the paths to folders within the package for importing modules, opening files etc
+pwd=os.path.dirname(os.path.abspath(__file__)) #get the directory this script is in
+dir_locust_io=os.path.dirname(pwd) #go one level up from that
+dir_input_files=os.path.join(dir_locust_io,'input_files/') #add the / at the end so the user only needs to append filenames
+dir_output_files=os.path.join(dir_locust_io,'output_files/')
+dir_classes=os.path.join(dir_locust_io,'classes/')
+
+
+
+#data which must exist to be able to run LOCUST
+required_equilibrium=['rdim','zdim','rcentr','rleft','zmid','rmaxis','zmaxis','simag','sibry','bcentr',
+'current','simag','rmaxis','zmaxis','sibry','xdum','fpol','pres','ffprime','pprime','psirz','qpsi','nbbbs','limitr','rbbbs','zbbbs']
+required_beam_deposition=['r','phi','z','v_r','v_phi','v_z']
+required_temperature=['flux_pol','T']
+required_number_density=['flux_pol','n']
 
 #################################
 
