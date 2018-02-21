ef3777c9946c77f165f58a2f54357ee0a259e52f
Samuel Ward <shw515@york.ac.uk>
2018-02-21 17:34:32 +0000

    backing up for the evening

--- processing/process_input.py
+++ processing/process_input.py
@@ -11,19 +11,57 @@ notes:
 ---
 '''
 
-##################################################################
+###################################################################################################
 #Preamble
 
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits import mplot3d #import 3D plotting axes
 from matplotlib import cm #get colourmaps
+import scipy.interpolate
 import scipy.integrate
 
 
 
-##################################################################
+
+###################################################################################################
 #Main Code
+
+
+################################################################## Supporting functions
+
+def interpolate_2D(x,y,X_grid,Y_grid,Z_grid):
+    """
+    interpolate a 2D grid to positions x,y
+
+    notes:
+        uses Rbf - https://stackoverflow.com/questions/37872171/how-can-i-perform-two-dimensional-interpolation-using-scipy
+    """
+
+    X_grid,Y_grid=np.meshgrid(X_grid,Y_grid)
+    interpolator=scipy.interpolate.Rbf(X_grid,Y_grid,grid,function='cubic',smooth=0)
+    interpolated_values=interpolator(x,y)
+
+    return interpolated_values
+
+def interpolate_1D(x,X_line,Y_line):
+    """
+    interpolate a 1D function to position x
+
+    notes:
+        uses Rbf - https://stackoverflow.com/questions/37872171/how-can-i-perform-two-dimensional-interpolation-using-scipy
+    """
+
+    interpolator=scipy.interpolate.Rbf(X_line,Y_line,grid,function='cubic',smooth=0)
+    interpolated_values=interpolator(x)
+
+    return interpolated_values
+
+
+
+
+
+################################################################## Specific functions
 
 def QTP_calc(Q=None,T=None,P=None):
     """
@@ -55,19 +93,22 @@ def QTP_calc(Q=None,T=None,P=None):
 
         T=scipy.integrate.cumtrapz(y=Q,x=P,initial=0) #0 here should be toroidal flux at the magnetic axis
 
+
         return T
 
     elif P is None: #need to caclulate P
-
         Q[Q==0.0]=0.0001 #replace zero values with small numbers to stop divide by zero
         P=scipy.integrate.cumtrapz(y=1/Q,x=T,initial=0) #0 here should be poloidal flux at the magnetic axis
 
         return P
 
-def grid_interpolate(x,y,grid)
+def B_from_flux_2D(psi_grid):
+    """
+    notes:
+    """
+    
 
-
-
+    return B_field
 
 #################################
 
