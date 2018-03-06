618e91e714547e3660a4ac755e2e61c913b6dd10
Samuel Ward <shw515@york.ac.uk>
2018-03-06 12:37:23 +0000

    now have example scripts plot annoying LOCUST ascii

--- example_project/example.py
+++ example_project/example.py
@@ -56,6 +56,15 @@ except:
 ##################################################################
 #Main Code
 
+print('''\n\n\n\
+88                                                          88              
+88                                                ,d        ""              
+88                                                88                    
+88  ,adPPYba,   ,adPPYba, 88       88 ,adPPYba, MM88MMM     88  ,adPPYba,   
+88 a8"     "8a a8"     "" 88       88 I8[    ""   88        88 a8"     "8a  
+88 8b       d8 8b         88       88  `"Y8ba,    88        88 8b       d8  
+88 "8a,   ,a8" "8a,   ,aa "8a,   ,a88 aa    ]8I   88,       88 "8a,   ,a8"  
+88  `"YbbdP"'   `"Ybbd8"'  `"YbbdP'Y8 `"YbbdP"'   "Y888 ____88  `"YbbdP"' \n\n\n''')
 
 #read in the sample input data
 my_Ne=input_classes.Number_Density(ID='LOCUST_IO sample Ne profile',data_format='ASCII',input_filename='sample_ne_profile.file',properties='electrons')
