58b6c0e0955b68075a919f8ffdcb53c8f321a6c0
Samuel Ward <shw515@york.ac.uk>
2018-04-11 07:08:38 +0200

    fixed object initialisations after they were calling obsolete input_filename class initialisation parameter

--- example_project/example.py
+++ example_project/example.py
@@ -70,16 +70,16 @@ print('''\n\n\n\
 
 
 #read in the sample input data
-my_Ne=input_classes.Number_Density(ID='LOCUST_IO sample Ne profile',data_format='ASCII',input_filename='sample_ne_profile.file',properties='electrons')
+my_Ne=input_classes.Number_Density(ID='LOCUST_IO sample Ne profile',data_format='ASCII',filename='sample_ne_profile.file',properties='electrons')
 
 #see what it looks like
 plot_input.plot_number_density(my_Ne)
 
 #read in the sample output data
-my_orbits=output_classes.Orbits(ID='LOCUST_IO sample orbits',data_format='ASCII',input_filename='sample_orbits.file')
+my_orbits=output_classes.Orbits(ID='LOCUST_IO sample orbits',data_format='ASCII',filename='sample_orbits.file')
 
 #plot particle 0's trajectory in the R,Z plane
-plot_output.plot_orbits(my_orbits,[0])
+plot_output.plot_orbits(my_orbits)
 
 
 #################################
