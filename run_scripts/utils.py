6633884026d9d34c5c592dd9beaf32add93a2cde
Samuel Ward <samuel.ward@york.ac.uk>
2018-08-24 18:50:00 +0100

    fixed typos in run_scripts.utils not importing numpy

--- run_scripts/utils.py
+++ run_scripts/utils.py
@@ -21,6 +21,7 @@ import sys #have global imports --> makes less modular (no "from input_classes i
 try:
     import os
     import subprocess
+    import numpy as np
 except:
     raise ImportError("ERROR: initial modules could not be imported!\nreturning")
     sys.exit(1)
