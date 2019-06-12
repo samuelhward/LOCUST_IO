93badbc3849ce5c76fda58e31d71fead67425b82
armoured-moose <samuel.ward@iter.org>
2019-06-12 14:17:12 +0200

    combined all writing to core_profiles IDS in /generate_test_IDS.py to avoid put() overwriting

--- src/run_scripts/generate_test_IDS.py
+++ src/run_scripts/generate_test_IDS.py
@@ -25,10 +25,11 @@ if __name__=='__main__':
         sys.exit(1)
 
 import numpy as np
+import imas
 
 from classes.input_classes.temperature import Temperature as T
-from classes.input_classes.number_density import Number_Density as ND
-from classes.input_classes.beam_deposition import Beam_Deposition as BD
+from classes.input_classes.core_profiles import core_profiles as ND
+from classes.input_classes.distribution_sources import distribution_sources as BD
 from classes.input_classes.equilibrium import Equilibrium as EQ
 from classes.input_classes.perturbation import Perturbation as P
 from classes.input_classes.wall import Wall as W
@@ -36,39 +37,45 @@ from classes.input_classes.wall import Wall as W
 ##################################################################
 #Main
 
-def generate_test_IDS(shot,run,use_temperature=True,use_number_density=True,
-    use_beam_deposition=True,use_equilibrium=True,use_perturbation=False,use_wall=False):
+def generate_test_IDS(shot,run,use_core_profiles=True,
+    use_distribution_sources=True,use_equilibrium=True,
+    use_mhd_linear=False,use_wall=False):
     """
     generate test IDS filled with dummy LOCUST input data
 
     notes:
     """
 
-    if use_temperature:
-        DUMMY_profile=np.linspace(1,100,100)
-        DUMMY_flux_pol=np.linspace(0,1,100)
-        temp_i=T(ID='LOCUST_IO test ion temperature',species='ions')
-        temp_e=T(ID='LOCUST_IO test electron temperature',species='electrons')
-        temp_i.set(flux_pol=DUMMY_flux_pol)
-        temp_i.set(T=DUMMY_profile)
-        temp_e.set(flux_pol=DUMMY_flux_pol)
-        temp_e.set(T=DUMMY_profile)
-        temp_i.dump_data(data_format='IDS',shot=shot,run=run,species='ions')
-        temp_e.dump_data(data_format='IDS',shot=shot,run=run,species='electrons')
+    test_IDS=imas.ids(shot,run)
+    test_IDS.open_env('wards2','locust_imas','3')
 
-    if use_number_density:
-        DUMMY_profile=np.linspace(1,100,100)
-        DUMMY_flux_pol=np.linspace(0,1,100)
-        numd_i=ND(ID='LOCUST_IO test ion density',species='ions')
-        numd_e=ND(ID='LOCUST_IO test electron density',species='electrons')
-        numd_i.set(flux_pol=DUMMY_flux_pol)
-        numd_i.set(n=DUMMY_profile)
-        numd_e.set(flux_pol=DUMMY_flux_pol)
-        numd_e.set(n=DUMMY_profile)
-        numd_i.dump_data(data_format='IDS',shot=shot,run=run,species='ions')
-        numd_e.dump_data(data_format='IDS',shot=shot,run=run,species='electrons')
+    if core_profiles:
 
-    if use_beam_deposition:
+        temperature_e=np.linspace(1000,1,100)
+        temperature_i=np.linspace(5000,1,100)
+        density=np.linspace(1e19,1e6,100)
+        flux_pol=np.linspace(0,1,100)
+
+        test_IDS.core_profiles.get()
+        test_IDS.core_profiles.code.version='1'
+        test_IDS.core_profiles.code.name="LOCUST_IO"
+        test_IDS.core_profiles.ids_properties.homogeneous_time=1   
+        test_IDS.core_profiles.ids_properties.comment='a comment'
+        test_IDS.core_profiles.time=np.array([0.0])
+
+        test_IDS.core_profiles.profiles_1d.resize(1)
+        test_IDS.core_profiles.profiles_1d[0].time=0.0
+        test_IDS.core_profiles.profiles_1d[0].ion.resize(1)
+        test_IDS.core_profiles.profiles_1d[0].ion[0].temperature=temperature_i
+        test_IDS.core_profiles.profiles_1d[0].ion[0].density=density
+        test_IDS.core_profiles.profiles_1d[0].grid.psi=flux_pol
+        test_IDS.core_profiles.profiles_1d[0].electrons.temperature=temperature_e
+        test_IDS.core_profiles.profiles_1d[0].electrons.density=density
+
+        test_IDS.core_profiles.put()
+
+    if use_distribution_sources:
+
         beam=BD(ID='LOCUST_IO test beam deposition')
         beam.set(R=np.random.uniform(size=10))
         beam.set(phi=np.random.uniform(size=10))
@@ -80,19 +87,21 @@ def generate_test_IDS(shot,run,use_temperature=True,use_number_density=True,
         beam.dump_data(data_format='IDS',shot=shot,run=run)
 
     if use_equilibrium:
+
         filename_equilibrium='sample_GEQDSK.file'
         equil=EQ(ID='LOCUST_IO test equilibrium',filename=filename_equilibrium,data_format='GEQDSK')
         equil.dump_data(data_format='IDS',shot=shot,run=run)
 
-    if use_perturbation:
-        filename_perturbation='BPLASMA_n3'
-        pert=P(ID='LOCUST_IO test perturbation')
+    if use_mhd_linear:
+        filename_mhd_linear='BPLASMA_n3'
+        pert=P(ID='LOCUST_IO test mhd_linear')
 
     if use_wall:
         filename_wall=''
         wall=W(ID='LOCUST_IO test wall')
 
-
+    test_IDS.close()
+    
 if __name__=='__main__':
 
     import argparse #take user command line input here
@@ -101,21 +110,20 @@ if __name__=='__main__':
     parser.add_argument('--shot',type=int,action='store',dest='shot',help="IMAS shot number",required=True)
     parser.add_argument('--run',type=int,action='store',dest='run',help="IMAS run number",required=True)
 
-    parser.add_argument('--temperature',type=str,action='store',default=True,dest='use_temperature',help="toggle whether to dump test temperature to IMAS",required=False)
-    parser.add_argument('--number_density',type=str,action='store',default=True,dest='use_number_density',help="toggle whether to dump test number_density to IMAS",required=False)
-    parser.add_argument('--beam_deposition',type=str,action='store',default=True,dest='use_beam_deposition',help="toggle whether to dump test beam_deposition to IMAS",required=False)
+    parser.add_argument('--core_profiles',type=str,action='store',default=True,dest='use_core_profiles',help="toggle whether to dump test core_profiles to IMAS",required=False)
+    parser.add_argument('--distribution_sources',type=str,action='store',default=True,dest='use_distribution_sources',help="toggle whether to dump test distribution_sources to IMAS",required=False)
     parser.add_argument('--equilibrium',type=str,action='store',default=True,dest='use_equilibrium',help="toggle whether to dump test equilibrium to IMAS",required=False)
-    parser.add_argument('--perturbation',type=str,action='store',default=False,dest='use_perturbation',help="toggle whether to dump test perturbation to IMAS",required=False)
+    parser.add_argument('--mhd_linear',type=str,action='store',default=False,dest='use_mhd_linear',help="toggle whether to dump test mhd_linear to IMAS",required=False)
     parser.add_argument('--wall',type=str,action='store',default=False,dest='use_wall',help="toggle whether to dump test wall to IMAS",required=False)
     
     args=parser.parse_args()
 
     #convert 
-    generate_test_IDS(shot=args.shot,run=args.run,use_temperature=args.use_temperature,
-        use_number_density=args.use_number_density,
-        use_beam_deposition=args.use_beam_deposition,
+    generate_test_IDS(shot=args.shot,run=args.run,
+        use_core_profiles=args.use_core_profiles,
+        use_distribution_sources=args.use_distribution_sources,
         use_equilibrium=args.use_equilibrium,
-        use_perturbation=args.use_perturbation,
+        use_mhd_linear=args.use_mhd_linear,
         use_wall=args.use_wall)
 
 #################################
