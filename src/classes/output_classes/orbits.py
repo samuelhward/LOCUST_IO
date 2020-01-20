3f6b0bd2649f8c725db5704ce0fc912e89d45fe3
armoured-moose <samuel.ward@iter.org>
2020-01-20 21:04:09 +0100

    fixed logic in orbits.py

--- src/classes/output_classes/orbits.py
+++ src/classes/output_classes/orbits.py
@@ -326,7 +326,7 @@ class Orbits(classes.base_output.LOCUST_output):
 
         if ndim==2: #2D plotting
 
-            elif axes==['X','Y']: #plotting top-down
+            if axes==['X','Y']: #plotting top-down
                 if 'X' not in self.data or 'Y' not in self.data:
                     try:
                         self['X'],self['Y']=processing.utils.RphiZ_to_XYZ(self['R'],self['phi'])
