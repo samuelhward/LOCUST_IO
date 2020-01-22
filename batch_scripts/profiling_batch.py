82a9ea7cd7e23113b94e02ee0cb91a5366b4b187
Samuel <samuel.ward@iter.org>
2020-01-22 23:02:52 +0100

    first commit of profiling study repo
    added context and initial batch files for varying threads, blocks and TIMAX
    assumes that user has set of input files within LOCUST dir_input

---
+++ batch_scripts/profiling_batch.py
@@ -0,0 +1,166 @@
+#profiling_batch.py
+ 
+"""
+Samuel Ward
+22/01/2019
+----
+script for controlling and launching LOCUST profiling workflow
+--- 
+notes:         
+    single species
+---
+"""
+
+###################################################################################################
+#Preamble
+ 
+import sys #have global imports --> makes less modular (no "from input_classes import x") but best practice to import whole input_classes module anyway
+
+try:
+    import numpy as np
+    import pathlib
+    import copy
+except:
+    raise ImportError("ERROR: initial modules could not be imported!\nreturning\n")
+    sys.exit(1)
+
+try:
+    import context
+except:
+    raise ImportError("ERROR: context.py could not be imported!\nreturning\n")
+    sys.exit(1)
+
+try:
+    from run_scripts.batch import Batch
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/run_scripts/batch.py could not be imported!\nreturning\n")
+    sys.exit(1)
+
+try:
+    import support
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/support.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+try:
+    import constants
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/constants.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+try:
+    import settings
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/settings.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+
+################################################################## 
+#Main 
+
+#define parameters which are fixed throughout a parameter scan - if we want to vary then add as a layer in the for loops
+LOCUST_run__system_name='TITAN'
+LOCUST_run__repo_URL=None
+LOCUST_run__commit_hash=None
+study_name='scan_threads_blocks'
+
+#################################
+#initialise args needed for Batch class
+
+args_batch={}
+args_batch['LOCUST_run__dir_LOCUST']=[]
+args_batch['LOCUST_run__dir_input']=[]
+args_batch['LOCUST_run__dir_output']=[]
+args_batch['LOCUST_run__dir_cache']=[]
+args_batch['LOCUST_run__system_name']=[]
+args_batch['LOCUST_run__repo_URL']=[]
+args_batch['LOCUST_run__commit_hash']=[]
+args_batch['LOCUST_run__settings_prec_mod']=[]
+args_batch['LOCUST_run__flags']=[]
+
+##################################################################
+#define variable parameters
+
+parameter__threads=np.array([2**n for n in [4,5,6]])
+parameter__blocks=np.array([2**n for n in [4,5,6,7,8,9,10,11,12]])
+parameter__timax=np.array([0.001,0.01,0.1,1.0])
+
+##################################################################
+#create every valid combination of parameter, returned in flat lists
+#use zip and nest levels to define specific combinations which cannot be varied
+
+for thread in parameter__threads:
+    for block in parameter__blocks:
+        for timax in parameter__timax:
+        
+            #create string holding varying parameters                                       
+            parameter_string=''
+            parameter_string+='_'.join(['{}_{}'.format(parameter,str(value)) for parameter,value in zip([
+                            'threads',
+                            'blocks',
+                            'timax'
+                            ],[
+                            thread,
+                            block,
+                            timax])])
+
+            LOCUST_run__flags={}
+            LOCUST_run__flags['TOKAMAK']=1
+            LOCUST_run__flags['LEIID']=8
+            LOCUST_run__flags['WLIST']=True
+            LOCUST_run__flags['BRELAX']=True
+            LOCUST_run__flags['UNBOR']=100
+            LOCUST_run__flags['OPENMESH']=True
+            LOCUST_run__flags['OPENTRACK']=True
+            LOCUST_run__flags['PFCMOD']=True
+            LOCUST_run__flags['NOPFC']=True
+            LOCUST_run__flags['TOKHEAD']=True
+            LOCUST_run__flags['JXB2']=True
+            LOCUST_run__flags['PROV']=True
+            LOCUST_run__flags['PITCHCUR']=True
+            LOCUST_run__flags['EBASE']=True
+            LOCUST_run__flags['UHST']=True
+            LOCUST_run__flags['LNLBT']=True
+            LOCUST_run__flags['GEQDSKFIX1']=True
+            LOCUST_run__flags['BP']=True
+            LOCUST_run__flags['TIMAX']='{}D0'.format(timax)
+            LOCUST_run__flags['B3D']=True
+            LOCUST_run__flags['B3D_EX']=True
+            LOCUST_run__flags['SMALLEQ']=True #XXX test whether we need this when using mesh
+            LOCUST_run__flags['NCOILS']=3
+            LOCUST_run__flags['STATIC']=True
+            LOCUST_run__settings_prec_mod={}
+            LOCUST_run__settings_prec_mod['Ab']='AD' 
+            LOCUST_run__settings_prec_mod['Zb']='+1.0_gpu' 
+            LOCUST_run__settings_prec_mod['file_tet']="'locust_wall'" 
+            LOCUST_run__settings_prec_mod['file_eqm']="'locust_eqm'" 
+            LOCUST_run__settings_prec_mod['threadsPerBlock']=thread
+            LOCUST_run__settings_prec_mod['blocksPerGrid']=block
+            LOCUST_run__settings_prec_mod['root']="'/tmp/wards2/{study}/{params}'".format(study=study_name,params=parameter_string)
+            LOCUST_run__settings_prec_mod['nnum']='[-3,-3,-3,-6,-6,-6]'
+            LOCUST_run__settings_prec_mod['nmde']=6
+            LOCUST_run__settings_prec_mod['phase']='[0.0_gpu,0.0_gpu,0.0_gpu,0.0_gpu,0.0_gpu,0.0_gpu]'
+            LOCUST_run__settings_prec_mod['omega']='[0.0_gpu,0.0_gpu,0.0_gpu,0.0_gpu,0.0_gpu,0.0_gpu]'
+            LOCUST_run__settings_prec_mod['fi']='[1.0_gpu]'
+            LOCUST_run__settings_prec_mod['Ai']='[AD]'
+            LOCUST_run__settings_prec_mod['Zi']='[+1.0_gpu]'
+            LOCUST_run__settings_prec_mod['nion']=1
+
+            args_batch['LOCUST_run__dir_LOCUST'].append(copy.deepcopy("'{}'".format(str(support.dir_locust / study_name / parameter_string))))
+            args_batch['LOCUST_run__dir_input'].append(copy.deepcopy("'{}'".format(str(support.dir_input_files / study_name))))
+            args_batch['LOCUST_run__dir_output'].append(copy.deepcopy("'{}'".format(str(support.dir_output_files / study_name / parameter_string))))
+            args_batch['LOCUST_run__dir_cache'].append(copy.deepcopy("'{}'".format(str(support.dir_cache_files / study_name)))) #one level less to pool cache files into same directory across simulations
+            args_batch['LOCUST_run__system_name'].append(copy.deepcopy(LOCUST_run__system_name))
+            args_batch['LOCUST_run__repo_URL'].append(copy.deepcopy(LOCUST_run__repo_URL))
+            args_batch['LOCUST_run__commit_hash'].append(copy.deepcopy(LOCUST_run__commit_hash))
+            args_batch['LOCUST_run__settings_prec_mod'].append(copy.deepcopy(LOCUST_run__settings_prec_mod))
+            args_batch['LOCUST_run__flags'].append(copy.deepcopy(LOCUST_run__flags))
+
+##################################################################
+#define and launch the batch scripts
+
+profiling_batch=Batch(**args_batch)
+profiling_batch.launch(workflow_filepath='profiling_workflow.py',system_name='TITAN')   
+
+#################################
+ 
+##################################################################
+ 
+###################################################################################################
---
+++ batch_scripts/profiling_workflow.py
@@ -0,0 +1,221 @@
+#profiling_workflow.py
+
+'''
+Samuel Ward
+22/01/2019
+----
+workflow designed for profiling LOCUST
+---
+notes: 
+
+    assumes all input files already located in locust dir_input
+---
+'''
+
+
+##################################################################
+#Preamble
+
+try:
+    import sys
+    import subprocess
+    import pathlib
+    import shlex
+    import copy
+    import numpy as np
+    import ast
+except:
+    raise ImportError("ERROR: initial modules could not be imported!\nreturning\n")
+    sys.exit(1)
+
+if __name__=='__main__':
+
+    try:
+        import context
+    except:
+        raise ImportError("ERROR: context.py could not be imported!\nreturning\n")
+        sys.exit(1)
+
+try:
+    import run_scripts.workflow
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/run_scripts/workflow.py could not be imported!\nreturning\n")
+    sys.exit(1)
+try:
+    import run_scripts.environment
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/run_scripts/environment.py could not be imported!\nreturning\n")
+    sys.exit(1)
+try:
+    from run_scripts.LOCUST_run import LOCUST_run
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/run_scripts/LOCUST_run.py could not be imported!\nreturning\n")
+    sys.exit(1)
+try:
+    import run_scripts.NEMO_run
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/run_scripts/NEMO_run.py could not be imported!\nreturning\n")
+    sys.exit(1)
+try:
+    from run_scripts.MARS_builder_run import MARS_builder_run
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/run_scripts/MARS_builder_run.py could not be imported!\nreturning\n")
+    sys.exit(1)
+try:
+    import run_scripts.utils
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/run_scripts/utils.py could not be imported!\nreturning\n")
+    sys.exit(1)
+try:
+    import processing.utils
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/processing/utils.py could not be imported!\nreturning\n")
+    sys.exit(1)
+
+try:
+    from classes.input_classes.temperature import Temperature
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/classes/input_classes/temperature.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+try:
+    from classes.input_classes.number_density import Number_Density
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/classes/input_classes/number_density.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+try:
+    from classes.input_classes.rotation import Rotation
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/classes/input_classes/rotation.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+try:
+    from classes.input_classes.equilibrium import Equilibrium
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/classes/input_classes/equilibrium.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+try:
+    from classes.input_classes.perturbation import Perturbation
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/classes/input_classes/perturbation.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+try:
+    from classes.input_classes.beam_deposition import Beam_Deposition
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/classes/input_classes/perturbation.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+try:
+    from classes.input_classes.wall import Wall
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/classes/input_classes/wall.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+
+try:
+    import support
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/support.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+try:
+    import settings
+except:
+    raise ImportError("ERROR: LOCUST_IO/src/settings.py could not be imported!\nreturning\n") 
+    sys.exit(1)
+
+##################################################################
+#Main
+
+class profiling_workflow(run_scripts.workflow.Workflow):
+    """
+    defines a workflow which runs a performance parameter scan
+
+    notes:
+    usage:
+    """ 
+
+    def __init__(self,
+                *args,
+                **kwargs):
+        """
+        notes:
+        """
+
+        super().__init__()
+
+        self.args={**kwargs}
+
+        #################################
+        #derive some information
+        
+        #################################
+        #add workflow stages
+
+        self.add_command(command_name='mkdir',command_function=self.setup_study_dirs,position=1) #add new workflow stages
+        self.add_command(command_name='run_LOCUST',command_function=self.run_LOCUST,position=2)
+        self.add_command(command_name='delete_outputs',command_function=self.delete_outputs,position=3)
+
+    def setup_study_dirs(self,*args,**kwargs):
+        """
+        notes:
+        """
+
+        for direc in [
+                        self.args['LOCUST_run__dir_input'],
+                        self.args['LOCUST_run__dir_output'],
+                        self.args['LOCUST_run__dir_cache']
+                        ]:
+            if not pathlib.Path(direc).is_dir(): pathlib.Path(direc).mkdir(parents=True)
+
+    def run_LOCUST(self,*args,**kwargs):
+        """
+        notes:
+        """
+
+        LOCUST_workflow=LOCUST_run(system_name=self.args['LOCUST_run__system_name'],
+            repo_URL=self.args['LOCUST_run__repo_URL'],
+            commit_hash=self.args['LOCUST_run__commit_hash'],
+            dir_locust=self.args['LOCUST_run__dir_LOCUST'],
+            dir_input=self.args['LOCUST_run__dir_input'],
+            dir_output=self.args['LOCUST_run__dir_output'],
+            dir_cache=self.args['LOCUST_run__dir_cache'],
+            settings_prec_mod=self.args['LOCUST_run__settings_prec_mod'],
+            flags=self.args['LOCUST_run__flags'])
+
+        LOCUST_workflow.run()
+
+    def delete_outputs(self,*args,**kwargs):
+        """
+        notes:
+        """
+
+        for file in pathlib.Path(self.args['LOCUST_run__dir_output']).glob('*'):
+            if 'rundata' not in str(file):
+                subprocess.run(shlex.split('rm {file}'.format(file=str(file))),shell=False) #delete all main outputs except rundata messages
+
+
+if __name__=='__main__':
+
+    import argparse
+    parser=argparse.ArgumentParser(description='run profiling_workflow from command line in Python!')
+    
+    parser.add_argument('--LOCUST_run__dir_LOCUST',type=str,action='store',dest='LOCUST_run__dir_LOCUST',help="",default=support.dir_locust)
+    parser.add_argument('--LOCUST_run__dir_input',type=str,action='store',dest='LOCUST_run__dir_input',help="",default=support.dir_input_files)
+    parser.add_argument('--LOCUST_run__dir_output',type=str,action='store',dest='LOCUST_run__dir_output',help="",default=support.dir_output_files)
+    parser.add_argument('--LOCUST_run__dir_cache',type=str,action='store',dest='LOCUST_run__dir_cache',help="",default=support.dir_cache_files)
+    parser.add_argument('--LOCUST_run__system_name',type=str,action='store',dest='LOCUST_run__system_name',help="",default='TITAN')
+    parser.add_argument('--LOCUST_run__repo_URL',type=str,action='store',dest='LOCUST_run__repo_URL',help="",default=settings.repo_URL_LOCUST)
+    parser.add_argument('--LOCUST_run__commit_hash',type=str,action='store',dest='LOCUST_run__commit_hash',help="",default=None)
+    parser.add_argument('--LOCUST_run__settings_prec_mod',nargs='+',type=str,action='store',dest='LOCUST_run__settings_prec_mod',help="",default={})
+    parser.add_argument('--LOCUST_run__flags',nargs='+',type=str,action='store',dest='LOCUST_run__flags',help="",default={})
+
+    args=parser.parse_args()
+
+    #provide some extra parsing steps to dict-like and array-like input arguments
+    args.LOCUST_run__settings_prec_mod=run_scripts.utils.command_line_arg_parse_dict(args.LOCUST_run__settings_prec_mod)
+    args.LOCUST_run__flags=run_scripts.utils.command_line_arg_parse_dict(args.LOCUST_run__flags)
+
+    workflow=profiling_workflow(**{key:arg for key,arg in args._get_kwargs()})
+    workflow.run()
+
+#################################
+
+##################################################################
+
+###################################################################################################
