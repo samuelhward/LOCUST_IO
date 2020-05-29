a72b8a105811963f5162aeac88b0bd049211e623
armoured-moose <samuel.ward@iter.org>
2020-05-29 01:18:08 +0100

    added PFC power fluxes to rundata class + overhaul

--- src/classes/output_classes/rundata.py
+++ src/classes/output_classes/rundata.py
@@ -56,27 +56,86 @@ except:
 
 ################################################################## Orbits functions
 
-def read_rundata_LOCUST(filepath,**properties):
+def read_rundata_LOCUST(filepath):
     """
     reads generic rundata file output from LOCUST
 
     notes:
-
     """
 
     print("reading rundata from LOCUST")
 
+    def escape_ansi(line):
+        import re
+        ansi_escape =re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
+        return ansi_escape.sub('', line)
+
+    file=open(filepath,'r')
+    lines=file.readlines()
+
+    rundata={}
+
+    for line in lines: #pre-parse the output data according to calling function, message or variable and value if any
+        try:
+            split_line=escape_ansi(line).strip('\n').split(':')
+            for column_number,column in enumerate(split_line):
+                if all([char==' ' for char in column]): del(split_line[column_number])
+
+            calling_function=split_line[0].strip()
+            variable_or_message=split_line[1].strip()
+            
+            if calling_function not in rundata:
+                rundata[calling_function]={}
+
+            try:
+                values=[value.strip() for value in split_line[2:]]
+                if variable_or_message not in rundata[calling_function]:
+                    rundata[calling_function][variable_or_message]=[values] 
+                else: #if key already occupied then append value
+                    rundata[calling_function][variable_or_message].append(values) 
+            except:
+                rundata[calling_function][variable_or_message]=None
+        except:            
+            pass
+
+    for calling_function in rundata:
+        for variable_or_message in rundata[calling_function]:
+            if len(rundata[calling_function][variable_or_message])==1: 
+                rundata[calling_function][variable_or_message]=rundata[calling_function][variable_or_message][0]
+
     input_data={} #initialise data dictionary
 
+    #extract power flux data
+    input_data['PFC_power']={}
+    input_data['PFC_power']['component']={}
+
+    for message in rundata['write_vtk']:
+
+        if 'Integrated power' in message:
+            if 'to component' in message:
+                for component_power in rundata['write_vtk'][message]:
+                    input_data['PFC_power']['component'][str(component_power[0])]=float(component_power[1][:-2])*1.e6
+            elif 'to PFCs' in message:
+                #print(message)
+                #print(rundata['write_vtk'][message])
+                input_data['PFC_power']['total']=float(rundata['write_vtk'][message][0][:-2])*1.e6
+
+    #extract loss channel data
+    input_data['loss_channels']={}
+
+    for message in rundata['loss_chk']:
+        try:
+            input_data['loss_channels'][message]=rundata['loss_chk'][message][0].replace(' ','').replace('MW','')
+            if '%' in input_data['loss_channels'][message]:
+                input_data['loss_channels'][message]=input_data['loss_channels'][message].replace('%','')
+            else:
+                input_data['loss_channels'][message]=float(input_data['loss_channels'][message])*1.e6
+        except:
+            pass
+
+    #look for kernel end time
     input_data['time_total']=None
-
-    with open(filepath,'r') as file: #loop through file here
-        for line in file:
-
-
-            if 'End of Kernel wrapper' in line: 
-                input_data['time_total']=float(line.split()[-1]) #extract time without units
-
+    input_data['time_total']=float(rundata['ctrk']['End of Kernel wrapper'][0])
 
     print("finished reading rundata from LOCUST")
 
@@ -107,7 +166,7 @@ class Rundata(classes.base_output.LOCUST_output):
 
     LOCUST_output_type='rundata'
 
-    def read_data(self,data_format=None,filename=None,shot=None,run=None,**properties):
+    def read_data(self,data_format=None,filename=None,**properties):
         """
         read rundata from file 
 
