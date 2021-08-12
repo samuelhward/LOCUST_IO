ed1db5a5de9175fcb55b0f233a5e6c321950b7fa
armoured-moose <samuelhward@gmail.com>
2021-08-12 21:54:25 +0100

    parallelised plotting script for all_scenarios study

--- scripts/studies/scan_upper_lower_all_scenarios/plot_loss_grid.py
+++ scripts/studies/scan_upper_lower_all_scenarios/plot_loss_grid.py
@@ -76,11 +76,28 @@ def calc_PFC_power(filename,classtype='fpl'):
         PFC_power=-1
     return PFC_power  
 
-PFC_power=templates.plot_mod.apply_func_parallel(calc_PFC_power,'fpl',batch_data,processes=24,chunksize=4)
+def calc_mean_loss_time(filename,classtype='fpl'):
+    fpl=templates.plot_mod.read_locust_io_obj(filename=filename,classtype=classtype)
+    if fpl:
+        i=np.where(fpl['status_flag']=='PFC_intercept_3D')[0]
+        mean_loss_time=np.average(fpl['time'][i],weights=fpl['FG'][i])
+    else:
+        mean_loss_time=-1
+    return mean_loss_time  
 
-PFC_power=np.array(PFC_power).reshape(len(batch_data.parameters__databases),len(batch_data.parameters__toroidal_mode_numbers),len(batch_data.parameters__phases_upper),len(batch_data.parameters__phases_lower))
+calc_function=calc_PFC_power
+title='Mean loss time (s)'
+title='NBI deposited power loss (%)'
 
-fig,ax=plt.subplots(len(batch_data.parameters__databases),len(batch_data.parameters__toroidal_mode_numbers)+2,constrained_layout=True) #add pair of axes for ploting XPD alongside
+
+
+
+
+
+quantity=templates.plot_mod.apply_func_parallel(calc_function,'fpl',batch_data,processes=24,chunksize=4)
+quantity=np.array(quantity).reshape(len(batch_data.parameters__databases),len(batch_data.parameters__toroidal_mode_numbers),len(batch_data.parameters__phases_upper),len(batch_data.parameters__phases_lower))
+
+fig,ax=plt.subplots(len(batch_data.parameters__databases),len(batch_data.parameters__toroidal_mode_numbers)+int(len(batch_data.parameters__toroidal_mode_numbers)/2.),constrained_layout=True) #add pair of axes for ploting XPD alongside
 ax=np.array(ax,ndmin=2)
 counter=0 #counter over different scenarios
 spectra_per_fundamental=2 #how many spectra are we looping over per fundamental mode number? e.g. [[3],[3,6]] is 2 spectra
@@ -90,38 +107,58 @@ for plasma_state_counter,database in enumerate(batch_data.parameters__databases)
         if len(mode_number)==1:
             n=int(np.abs(mode_number[0]))
             case=int(batch_data.parameters__databases[plasma_state_counter][-1])
-            plot_X_point_displacement(case=case,n=n,LVV=90,fig=fig,ax=ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+spectra_per_fundamental],colourbar=False,coord_system='ITER') #use MARS system because contours might be generated differently if using ITER system, so points might not match up
-            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+spectra_per_fundamental].set_xlim([np.min(batch_data.parameters__phases_lowers[mode_number_counter]),np.max(batch_data.parameters__phases_lowers[mode_number_counter])])
-            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+spectra_per_fundamental].set_ylim([np.min(batch_data.parameters__phases_uppers[mode_number_counter]),np.max(batch_data.parameters__phases_uppers[mode_number_counter])])
-            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+spectra_per_fundamental].set_xlabel('')
-            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+spectra_per_fundamental].set_ylabel('')
-            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+spectra_per_fundamental].set_xticks(batch_data.parameters__phases_lowers[mode_number_counter])
-            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+spectra_per_fundamental].set_yticks(batch_data.parameters__phases_uppers[mode_number_counter])
-            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+spectra_per_fundamental].set_xlim([np.min(batch_data.parameters__phases_lowers[mode_number_counter]),np.max(batch_data.parameters__phases_lowers[mode_number_counter])])
-            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+spectra_per_fundamental].set_ylim([np.min(batch_data.parameters__phases_uppers[mode_number_counter]),np.max(batch_data.parameters__phases_uppers[mode_number_counter])])
+            plot_X_point_displacement(case=case,n=n,LVV=90,fig=fig,ax=ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)],colourbar=False,coord_system='ITER', #use MARS system because contours might be generated differently if using ITER system, so points might not match up
+            plot_points=False
+            ) 
+            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_xlim([np.min(batch_data.parameters__phases_lowers[mode_number_counter]),np.max(batch_data.parameters__phases_lowers[mode_number_counter])])
+            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_ylim([np.min(batch_data.parameters__phases_uppers[mode_number_counter]),np.max(batch_data.parameters__phases_uppers[mode_number_counter])])
+            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_xlabel('')
+            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_ylabel('')
+            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_xticks(batch_data.parameters__phases_lowers[mode_number_counter][::2])
+            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_yticks(batch_data.parameters__phases_uppers[mode_number_counter][::2])
+            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_xlim([np.min(batch_data.parameters__phases_lowers[mode_number_counter]),np.max(batch_data.parameters__phases_lowers[mode_number_counter])])
+            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_ylim([np.min(batch_data.parameters__phases_uppers[mode_number_counter]),np.max(batch_data.parameters__phases_uppers[mode_number_counter])])
+
+            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].tick_params(
+                axis='both',          
+                which='both',      
+                labelbottom=False,
+                labelleft=True,
+                )
 
         dLPHASE,dUPHASE=batch_data.parameters__phases_lowers[mode_number_counter][1]-batch_data.parameters__phases_lowers[mode_number_counter][0],batch_data.parameters__phases_uppers[mode_number_counter][1]-batch_data.parameters__phases_upper[0]
         LPHASE,UPHASE=np.append(batch_data.parameters__phases_lowers[mode_number_counter],batch_data.parameters__phases_lowers[mode_number_counter][-1]+dLPHASE),np.append(batch_data.parameters__phases_uppers[mode_number_counter],batch_data.parameters__phases_uppers[mode_number_counter][-1]+dUPHASE)
         #settings.discrete_colmap(colmap_name='Greys',face_colour='red',number_bins=11)
-        mesh=ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].pcolormesh(LPHASE-dLPHASE/2.,UPHASE-dUPHASE/2.,100*PFC_power[plasma_state_counter,mode_number_counter]/Pinj,cmap='Greys',edgecolor='none',antialiased=True,vmin=0,vmax=10)
-        #mesh=ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].contourf(LPHASE[0:-1]-dLPHASE/2.,UPHASE[0:-1]-dUPHASE/2.,100*PFC_power[plasma_state_counter,mode_number_counter]/Pinj,cmap=settings.discrete_colmap(colmap_name='Greys',face_colour='black',number_bins=11),edgecolor='none',antialiased=True)#,vmin=0,vmax=10)
-        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_xticks(batch_data.parameters__phases_lowers[mode_number_counter])
-        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_yticks(batch_data.parameters__phases_uppers[mode_number_counter])
-        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_xlim([np.min(batch_data.parameters__phases_lowers[mode_number_counter]),np.max(batch_data.parameters__phases_lowers[mode_number_counter])])
-        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_ylim([np.min(batch_data.parameters__phases_uppers[mode_number_counter]),np.max(batch_data.parameters__phases_uppers[mode_number_counter])])
-        #ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].text(x=0.05,y=0.1,s=r'Pr={}, $\tau_{{\Phi}}/\tau_{{\mathrm{{E}}}}={}, \mathrm{{NBI}}_{{1}},\mathrm{{NBI}}_{{2}}$=[{},{}], $n$={}'.format(batch_data.parameters__kinetic_profs_Pr[0],batch_data.parameters__kinetic_profs_tF_tE[0],batch_data.config_beam_1,batch_data.config_beam_2,mode_number),horizontalalignment='left',transform=ax[plasma_state_counter,mode_number_counter].transAxes,color=settings.cmap_w(0.))
-        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_xlabel('')
-        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_ylabel('')
-        if mode_number_counter==0:
-            ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_ylabel('$\Delta\Phi_{\mathrm{U}}$ [deg]')
+        mesh=ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+1].pcolormesh(LPHASE-dLPHASE/2.,UPHASE-dUPHASE/2.,quantity[plasma_state_counter,mode_number_counter],cmap='Greys',edgecolor='none',antialiased=True)
+        #mesh=ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+1].contourf(LPHASE[0:-1]-dLPHASE/2.,UPHASE[0:-1]-dUPHASE/2.,quantity[plasma_state_counter,mode_number_counter],cmap=settings.discrete_colmap(colmap_name='Greys',face_colour='black',number_bins=11),edgecolor='none',antialiased=True)#,vmin=0,vmax=10)
+        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+1].set_xticks(batch_data.parameters__phases_lowers[mode_number_counter][::2])
+        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+1].set_yticks(batch_data.parameters__phases_uppers[mode_number_counter][::2])
+        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+1].set_xlim([np.min(batch_data.parameters__phases_lowers[mode_number_counter]),np.max(batch_data.parameters__phases_lowers[mode_number_counter])])
+        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+1].set_ylim([np.min(batch_data.parameters__phases_uppers[mode_number_counter]),np.max(batch_data.parameters__phases_uppers[mode_number_counter])])
+        #ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+1].text(x=0.05,y=0.1,s=r'Pr={}, $\tau_{{\Phi}}/\tau_{{\mathrm{{E}}}}={}, \mathrm{{NBI}}_{{1}},\mathrm{{NBI}}_{{2}}$=[{},{}], $n$={}'.format(batch_data.parameters__kinetic_profs_Pr[0],batch_data.parameters__kinetic_profs_tF_tE[0],batch_data.config_beam_1,batch_data.config_beam_2,mode_number),horizontalalignment='left',transform=ax[plasma_state_counter,mode_number_counter].transAxes,color=settings.cmap_w(0.))
+        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+1].set_xlabel('')
+        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+1].set_ylabel('')
+        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+1].set_title('EP losses: '+'$n$ = '+','.join(str(abs(mode)) for mode in mode_number))
+        ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)+1].tick_params(
+            axis='both',          
+            which='both',      
+            labelbottom=False,
+            labelleft=False,
+            )
 
-        #ax[plasma_state_counter,mode_number_counter+int(mode_number_counter/2)].set_title(f'{database}')
+
+for axis in ax[:,0]:
+    axis.set_ylabel('$\Delta\Phi_{\mathrm{U}}$ [deg]')
 
 for axis in ax[-1,:]:
     axis.set_xlabel('$\Delta\Phi_{\mathrm{L}}$ [deg]')
+    axis.tick_params(
+        axis='both',          
+        which='both',      
+        labelbottom=True,
+        )
 
-
-fig.suptitle('Total NBI power loss (%)')
+fig.suptitle(title)
 fig.colorbar(mesh,ax=ax.ravel().tolist())
 #fig.set_tight_layout(True)  
 plt.show()
