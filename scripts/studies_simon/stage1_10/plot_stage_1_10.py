8b5efe855242fb4524d61352ddaeaedbc6c5a568
armoured-moose <samuelhward@gmail.com>
2021-09-08 11:41:45 +0100

    1.10 plot script making paper 2 plots

--- scripts/studies_simon/stage1_10/plot_stage_1_10.py
+++ scripts/studies_simon/stage1_10/plot_stage_1_10.py
@@ -61,48 +61,56 @@ except:
 ################################################################## 
 #Main 
 
+cmap_r=settings.colour_custom([194,24,91,1])
+cmap_g=settings.colour_custom([76,175,80,1])
+cmap_b=settings.colour_custom([33,150,243,1])
+
 import stage_1_10_launch as batch_data
 
-outputs=templates.plot_mod.get_output_files(batch_data,'fpl')
+#outputs=templates.plot_mod.get_output_files(batch_data,'fpl')
 
 Pinj=33.e6
-PFC_power=[]
-for output in outputs:
-    if output:
-        i=np.where(output['status_flag']=='PFC_intercept_3D')[0]
-        PFC_power.append([1.e6*output['f']*np.sum((output['V_R'][i]**2+output['V_phi'][i]**2+output['V_Z'][i]**2)*output['FG'][i])*0.5*constants.mass_deuteron])
+def calc_PFC_power(filename,classtype='fpl'):
+    fpl=templates.plot_mod.read_locust_io_obj(filename=filename,classtype=classtype)
+    if fpl:
+        i=np.where(fpl['status_flag']=='PFC_intercept_3D')[0]
+        PFC_power=1.e6*fpl['f']*np.sum((fpl['V_R'][i]**2+fpl['V_phi'][i]**2+fpl['V_Z'][i]**2)*fpl['FG'][i])*0.5*constants.mass_deuteron
     else:
-        PFC_power.append([-10.])
+        PFC_power=-1
+    return PFC_power  
 
-'''
-outputs=templates.plot_mod.get_output_files(batch_data,'rund')
-
-Pinj=33.e6
-PFC_power=[]
-for output in outputs:
-    if output is not None and output['run_status']=='completed':
-        PFC_power.append([output['PFC_power']['total']])
-    else:
-        PFC_power.append([-10.])
-
-'''
-
-PFC_power=np.array(PFC_power).reshape(len(batch_data.parameters__kinetic_profs_Pr),len(batch_data.parameters__toroidal_mode_numbers),len(batch_data.parameters__phases_upper))
+PFC_power=templates.plot_mod.apply_func_parallel(calc_PFC_power,'fpl',batch_data,processes=24,chunksize=1)
+PFC_power=np.array(PFC_power).reshape(
+                    len(batch_data.parameters__kinetic_profs_Pr),
+                    len(batch_data.parameters__currents_upper),
+                    len(batch_data.parameters__toroidal_mode_numbers),
+                    len(batch_data.parameters__phases_upper)
+                    )
 
 # one figure per plasma
 fig,axs=plt.subplots(len(batch_data.parameters__kinetic_profs_Pr),constrained_layout=True)
-colours=['r','b'] #one colour per mode number
+colours=[cmap_r(0.),cmap_r(0.),cmap_b(0.),cmap_b(0.)] #one colour per mode number
+colours=['r','r',cmap_b(0.),cmap_b(0.)] #one colour per mode number
 
-for plasma_state_counter,(ax,power,Pr,tftE) in enumerate(zip([axs],PFC_power,batch_data.parameters__kinetic_profs_Pr,batch_data.parameters__kinetic_profs_tF_tE)):
-    for mode_number_counter,(mode_number,colour) in enumerate(zip(batch_data.parameters__toroidal_mode_numbers,colours)):
-        #find relative phase between coils for this toroidal mode number
-        relative_phases_upper_middle=batch_data.parameters__phases_uppers[mode_number_counter]-batch_data.parameters__phases_middles[mode_number_counter]
-        relative_phases_lower_middle=batch_data.parameters__phases_lowers[mode_number_counter]-batch_data.parameters__phases_middles[mode_number_counter]
-        ax.scatter(np.abs(mode_number[0])*(batch_data.parameters__phases_middles[mode_number_counter]),100*power[mode_number_counter]/Pinj,label=f'$n$ = {mode_number}, U:M={int(np.abs(mode_number[0])*relative_phases_upper_middle[0])}, L:M={int(np.abs(mode_number[0])*relative_phases_lower_middle[0])}',color=colour)
+for plasma_state_counter,(ax,Pr,tftE) in enumerate(zip([axs],batch_data.parameters__kinetic_profs_Pr,batch_data.parameters__kinetic_profs_tF_tE)):
+    for current_counter,(current,linestyle) in enumerate(zip(batch_data.parameters__currents_upper,['solid','solid'])):
+        for mode_number_counter,(mode_number,colour) in enumerate(zip(batch_data.parameters__toroidal_mode_numbers,colours)):
+            marker='.' if len(mode_number)>1 else 'x' #XXX if only one value of current, here override what linestyle denotes
+
+            label=f'$n$ = {"+".join(str(abs(int(mode))) for mode in mode_number)}'
+            #label+=', '+r'$\Delta\Phi_{\mathrm{u,m}}$'
+            #label+=f'={int(np.abs(mode_number[0])*relative_phases_upper_middle[0])}'
+            #label+=', '+r'$\Delta\Phi_{\mathrm{l,m}}$='
+            #label+=f'{int(np.abs(mode_number[0])*relative_phases_lower_middle[0])}'
+            
+            #find relative phase between coils for this toroidal mode number
+            relative_phases_upper_middle=batch_data.parameters__phases_uppers[mode_number_counter]-batch_data.parameters__phases_middles[mode_number_counter]
+            relative_phases_lower_middle=batch_data.parameters__phases_lowers[mode_number_counter]-batch_data.parameters__phases_middles[mode_number_counter]
+            ax.scatter(batch_data.parameters__phases_middles[mode_number_counter],100*PFC_power[plasma_state_counter,current_counter,mode_number_counter]/Pinj,label=label,color=colour,linestyle=linestyle,marker=marker,s=80)
 
 ax.legend()
-ax.set_xlabel('Relative rigid phase $\Delta\phi$',fontsize=20) #\Phi for absolute
-ax.set_ylabel('Deposited power lost [%]',fontsize=20)
+ax.set_xlabel('$\Phi_{\mathrm{m}}$ [deg]') #\Phi for absolute
+ax.set_ylabel('NBI power loss [%]')
 # remove ticks from total ax
 #ax_total = fig.add_subplot(111,frameon=False)
 #ax_total.tick_params(axis='both',which='both',bottom=False,labelbottom=False,left=False,labelleft=False)
